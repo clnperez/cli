3e997cd596ab40419dd36c7890795d6747f53922
Christy Perez <christy@linux.vnet.ibm.com>
2017-07-07 14:21:14 -0500

    savepoint before changing directions
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- cli/manifest/saver/saver.go
+++ cli/manifest/saver/saver.go
@@ -12,11 +12,14 @@ import (
 
 	climanifest "github.com/docker/cli/cli/manifest"
 	"github.com/docker/distribution/manifest/manifestlist"
+	digest "github.com/opencontainers/go-digest"
+	ociv1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 func ManifestSaveFromArchives(outFile string, archives []string) error {
 	fmt.Println("Save from archives: %s", archives)
 
+	index := ociv1.Index{}
 	platforms := make(map[string]manifestlist.PlatformSpec)
 
 	// Loop through archives, untar, look in manifest.json to get platform info
@@ -27,22 +30,43 @@ func ManifestSaveFromArchives(outFile string, archives []string) error {
 	fmt.Println(pwd)
 
 	for _, archive := range archives {
-		platform, err := getPlatform(filepath.Join(pwd, archive))
+		img, err := getImage(filepath.Join(pwd, archive))
+		if err != nil {
+			return err
+		}
+		platform, err := getPlatform(img)
 		if err != nil {
 			return err
 		}
 		platforms[archive] = platform
-		fmt.Println(platform)
+		//fmt.Println(platform)
 	}
 	// then make manifest list spec json
 	for archive, platform := range platforms {
-		fmt.Println("Key:", archive, "Value:", platform)
+		//fmt.Println("Key:", archive, "Value:", platform)
+		d := digest.Digest(md.Digest)
 	}
 	// then make bundle with original tars and manifest list spec inside
 	return nil
 }
 
-func getPlatform(archive string) (spec manifestlist.PlatformSpec, err error) {
+func getPlatform(img *climanifest.Image) (spec manifestlist.PlatformSpec, err error) {
+
+	buff := make([]byte, 500)
+	_, err = tr.Read(buff)
+	if err != nil {
+		return spec, err
+	}
+	if err := json.Unmarshal(buff, img); err != nil {
+		return spec, err
+	}
+	spec.Architecture = img.Architecture
+	spec.OS = img.OS
+	return spec, nil
+
+}
+
+func getImage(archive string) (*climanifest.Image, error) {
 	var (
 		img climanifest.Image
 	)
@@ -67,16 +91,13 @@ func getPlatform(archive string) (spec manifestlist.PlatformSpec, err error) {
 		buff := make([]byte, hdr.Size)
 		_, err = tr.Read(buff)
 		if err != nil {
-			return spec, err
+			return img, err
 		}
 		if err := json.Unmarshal(buff, &img); err != nil {
 			return spec, err
 		}
-		spec.Architecture = img.Architecture
-		spec.OS = img.OS
-		return spec, nil
 	}
-	return spec, nil
+	return &img, nil
 }
 
 func ManifestSaveLocalImages(images []string) error {
