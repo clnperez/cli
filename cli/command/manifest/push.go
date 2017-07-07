4906c2545fd56eb6f7e6b73661edcfb737d4bd24
Christopher Jones <tophj@linux.vnet.ibm.com>
2017-07-07 09:54:53 -0400

    init tests2
    Signed-off-by: Christopher Jones <tophj@linux.vnet.ibm.com>

--- cli/command/manifest/push.go
+++ cli/command/manifest/push.go
@@ -87,7 +87,7 @@ func newPushListCommand(dockerCli command.Cli) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "push [newRef | --file pre-annotated-yaml]",
 		Short: "Push a manifest list for an image to a repository",
-		Args:  cli.RequiresMinArgs(0),
+		Args:  cli.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return putManifestList(dockerCli, opts, args)
 		},
@@ -110,15 +110,6 @@ func putManifestList(dockerCli command.Cli, opts pushOpts, args []string) error 
 		yamlInput                         YamlInput
 		err                               error
 	)
-
-	// First get all the info we'll need from either a yaml file, or a user's locally-creatd manifest transaction.
-	numArgs := len(args)
-	if numArgs > 1 {
-		return fmt.Errorf("more than one argument provided to 'manifest push'")
-	}
-	if (numArgs == 0) && (opts.file == "") {
-		return fmt.Errorf("please push using a yaml file or a list created using 'manifest create.'")
-	}
 
 	fullTargetRef, targetRef, bareRef, err = constructTargetRefs(args, opts)
 	if err != nil {
