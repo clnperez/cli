77748cf3639857f36fdf53366d5678c492b86091
Christy Perez <christy@linux.vnet.ibm.com>
2017-10-20 10:29:53 -0500

    ignore interfacer for reference.Named
    It's not related to opts.NamedOpt. It just happens to have a Name()
    func.
    
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- cli/command/manifest/push.go
+++ cli/command/manifest/push.go
@@ -189,6 +189,7 @@ func buildBlobRequestList(imageManifest types.ImageManifest, repoName reference.
 	return blobReqs, nil
 }
 
+// nolint: interfacer
 func buildPutManifestRequest(imageManifest types.ImageManifest, targetRef reference.Named) (mountRequest, error) {
 	refWithoutTag, err := reference.WithName(targetRef.Name())
 	if err != nil {
